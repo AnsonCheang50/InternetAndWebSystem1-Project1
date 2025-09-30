dfabb9bab570dcd1c7a644dd95985f21c696df95
AnsonCheang50 <113309258+AnsonCheang50@users.noreply.github.com>
2025-09-30 19:14:23 -0400

    added wow feature

---
+++ javascript/projects.js
@@ -0,0 +1,7 @@
+/*The javascript came entirely from 
+https://www.w3schools.com/howto/howto_js_popup.asp  */
+
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
